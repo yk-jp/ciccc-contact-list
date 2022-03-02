2079b5d24cb130824daa8cc3f3e38cfdc694b580
yk-jp <k82.y12@gmail.com>
2022-03-02 14:46:30 -0800

    do tasks2

--- src/classes/Controller.java
+++ src/classes/Controller.java
@@ -2,5 +2,18 @@ package classes;
 
 public class Controller {
 
+    public void start() {
+        boolean isTerminated = false;
 
+        while(!isTerminated) {
+            isTerminated = this.quit();
+        }
+    }
+
+
+
+    private boolean quit() {
+        System.out.println("Bye!");
+        return true;
+    }
 }
